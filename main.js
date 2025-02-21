30cde7b8b9c4a749a84987d82ddbb3c2ce289d30
Nazaropo <144667191+Nazaropo@users.noreply.github.com>
2025-02-21 21:09:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+// let temperatureInCelsius;
+// let temperatureInFahrenheit;
+
+// temperatureInCelsius = 50;
+// temperatureInFahrenheit = (9 / 5) * temperatureInCelsius + 32;
+
+// console.log("hasil:", temperatureInFahrenheit);
+
+// temperatureInCelsius = 70;
+// temperatureInFahrenheit = (9 / 5) * temperatureInCelsius + 32;
+
+// console.log("hasil:", temperatureInFahrenheit);
+
+// temperatureInCelsius = 100;
+// temperatureInFahrenheit = (9 / 5) * temperatureInCelsius + 32;
+
+// console.log("hasil:", temperatureInFahrenheit);
+
+//parameter dan argument
+// function convertCelsiusToFahrenheit (temperature) {
+// const result= 9 / 5* temperature + 32;
+// return result;
+// }
+// const temperatureInFahrenheit = convertCelsiusToFahrenheit(90);
+// console.log("Hasilnya:", temperatureInFahrenheit);
+    
+
+// function sumNumbers(a, b) {
+//     const result = a + b;
+//     const result2 = a - b;
+//     const result3 = a * b;
+//     const result4 = a / b;
+//     return result;
+//     return result2;
+//     return result3;
+//     return result4;
+// }
+//     const result = sumNumbers(10, 5);
+//     console.log("10 + 5 =", result);
+// const result2 = sumNumbers(10, 5);
+// console.log("10 - 5 =", result);
+// const result3 = sumNumbers(10, 5);
+// console.log("10 * 5 =", result);
+// const result4 = sumNumbers(10, 5);
+// console.log("10 / 5 =", result);
+
+// function generateGreetingWorldMessage() {
+//   return "Halo, dunia!";
+//   console.log("Aku tidak akan tampil!");
+// }
+
+// const message = generateGreetingWorldMessage();
+// console.log(message);
